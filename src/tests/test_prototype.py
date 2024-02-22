6d9f4ce987a92f5e92abeb2da134920f1aa95b09
grufe <erikjosefsimonson@gmail.com>
2024-02-22 17:27:51 +0100

    Add directory for testing the prototype

---
+++ src/tests/test_prototype.py
@@ -0,0 +1,10 @@
+import pytest
+from .prototype.prototype import analyze_dependency_score
+
+if __name__ == "__main__":
+    print("Hello World!")
+
+def test_analyze_dependency_score():
+    git_urls = ["https://github.com/OSSQA-PUM/OSSQA"]
+
+
