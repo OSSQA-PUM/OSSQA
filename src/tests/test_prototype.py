351001c68faaf1ca0d7dec8ada3719ba2b0d21b0
grufe <erikjosefsimonson@gmail.com>
2024-02-26 09:57:00 +0100

    Add test for format of SSF scorecard

--- src/tests/test_prototype.py
+++ src/tests/test_prototype.py
@@ -1,10 +1,38 @@
 import pytest
-from .prototype.prototype import analyze_dependency_score
+from prototype.prototype import analyze_dependency_score
+from typing import List, Tuple
 
-if __name__ == "__main__":
-    print("Hello World!")
+@pytest.fixture
+def expected_scorecard_output() -> Tuple:
+    return ('Binary-Artifacts', 
+            'Branch-Protection', 
+            'CI-Tests', 
+            'CII-Best-Practices', 
+            'Code-Review', 
+            'Contributors', 
+            'Dangerous-Workflow', 
+            'Dependency-Update-Tool', 
+            'Fuzzing', 
+            'License', 
+            'Maintained', 
+            'Packaging', 
+            'Pinned-Dependencies', 
+            'SAST', 
+            'Security-Policy', 
+            'Signed-Releases', 
+            'Token-Permissions', 
+            'Vulnerabilities')
 
-def test_analyze_dependency_score():
-    git_urls = ["https://github.com/OSSQA-PUM/OSSQA"]
+def test_analyze_dependency_score(expected_scorecard_output) -> None:
+    # Tests the format of of the SSF scorecard analyser.
+    
+    git_urls = ["github.com/OSSQA-PUM/OSSQA"]
+
+    list : List[Tuple[str, int, str]] = analyze_dependency_score(git_urls[0])
+    assert len(list) == 18
+    assert len(list[0]) == 3
+    for item in list:
+        assert item[0] in expected_scorecard_output
+
 
 
