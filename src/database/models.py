3bed9d4d5f0e608eb601c7e94be5915908aeec18
Viktor Holta <viktor.holta@outlook.com>
2024-02-28 22:06:03 +0100

    Move database models to separate file

---
+++ src/database/models.py
@@ -0,0 +1,56 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+dependency_sbom = db.Table('dependency_sbom',
+                           db.Column('dependency_repo_commit', db.Integer, db.ForeignKey('dependency.repo_commit'),
+                                     primary_key=True),
+                           db.Column('sbom_serialNumber', db.Integer, db.ForeignKey('sbom.serialNumber'),
+                                     primary_key=True)
+                           )
+
+
+class Dependency(db.Model):
+    repo_commit = db.Column(db.String(60), primary_key=True)
+    score = db.Column(db.Integer, unique=False)
+    date_added = db.Column(db.DateTime, default=db.func.current_timestamp())
+
+    sboms = db.relationship('SBOM', secondary=dependency_sbom, lazy='subquery',
+                            back_populates='dependencies')
+    checks = db.relationship('DependencyCheck', backref='dependency', lazy=True)
+
+    def to_dict(self):
+        return {'repo_commit': self.repo_commit,
+                'score': self.score,
+                'name': self.name,
+                'version': self.version,
+                'date_added': self.date_added,
+                }
+
+
+class DependencyCheck(db.Model):
+    details = db.Column(db.String(60), primary_key=True)
+    score = db.Column(db.Double, unique=False)
+    reason = db.Column(db.String(60), unique=False)
+    name = db.Column(db.String(60), unique=False)
+    dependency_repo = db.Column(db.String(60), db.ForeignKey('dependency.repo_commit'), unique=False)
+
+
+    def to_dict(self):
+        return {'details': self.details,
+                'score': self.score,
+                'reason': self.reason,
+                'name': self.name,
+                }
+
+
+class SBOM(db.Model):
+    serialNumber = db.Column(db.String(60), primary_key=True)
+    version = db.Column(db.String(60), unique=False)
+
+    dependencies = db.relationship('Dependency', secondary=dependency_sbom, lazy='subquery', back_populates='sboms')
+
+    def to_dict(self):
+        return {'serialNumber': self.serialNumber,
+                'version': self.version,
+                }
--- src/database/server.py
+++ src/database/server.py
@@ -1,63 +1,12 @@
 from flask import Flask, jsonify, request
-from flask_sqlalchemy import SQLAlchemy
+
+from models import db
 
 app = Flask(__name__)
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./our.db'
-db = SQLAlchemy(app)
+db.init_app(app)
 
-dependency_sbom = db.Table('dependency_sbom',
-                           db.Column('dependency_repo_commit', db.Integer, db.ForeignKey('dependency.repo_commit'),
-                                     primary_key=True),
-                           db.Column('sbom_serialNumber', db.Integer, db.ForeignKey('sbom.serialNumber'),
-                                     primary_key=True)
-                           )
-
-
-class Dependency(db.Model):
-    repo_commit = db.Column(db.String(60), primary_key=True)
-    score = db.Column(db.Integer, unique=False)
-    date_added = db.Column(db.DateTime, default=db.func.current_timestamp())
-
-    sboms = db.relationship('SBOM', secondary=dependency_sbom, lazy='subquery',
-                            back_populates='dependencies')
-    checks = db.relationship('DependencyCheck', backref='dependency', lazy=True)
-
-    def to_dict(self):
-        return {'repo_commit': self.repo_commit,
-                'score': self.score,
-                'name': self.name,
-                'version': self.version,
-                'date_added': self.date_added,
-                }
-
-
-class DependencyCheck(db.Model):
-    details = db.Column(db.String(60), primary_key=True)
-    score = db.Column(db.Double, unique=False)
-    reason = db.Column(db.String(60), unique=False)
-    name = db.Column(db.String(60), unique=False)
-    dependency_repo = db.Column(db.String(60), db.ForeignKey('dependency.repo_commit'), unique=False)
-
-
-    def to_dict(self):
-        return {'details': self.details,
-                'score': self.score,
-                'reason': self.reason,
-                'name': self.name,
-                }
-
-
-class SBOM(db.Model):
-    serialNumber = db.Column(db.String(60), primary_key=True)
-    version = db.Column(db.String(60), unique=False)
-
-    dependencies = db.relationship('Dependency', secondary=dependency_sbom, lazy='subquery', back_populates='sboms')
-
-    def to_dict(self):
-        return {'serialNumber': self.serialNumber,
-                'version': self.version,
-                }
+from models import Dependency, DependencyCheck, SBOM
 
 
 @app.errorhandler(404)
