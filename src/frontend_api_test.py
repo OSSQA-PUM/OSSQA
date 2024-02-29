d05275a4bff3e8be9b6e92b3ef690c3ef07fae55
Simon Karlsson <simonkarlsson020314@gmail.com>
2024-02-29 11:21:14 +0100

    First version of frontend_api written. Can check for correct user priorities and check format of SBOM

---
+++ src/frontend_api_test.py
@@ -0,0 +1,6 @@
+import frontend_api
+
+frontend_api.frontend_api(path = "src/prototype/example-SBOM.json",  build_risk_assessment = 1,\
+                source_risk_assessment = 2,\
+                maintence = 1,\
+                continuous_testing = 1, code_vunerabilities = 10)
