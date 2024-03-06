026645f5e6d3f5aa7ba0e4d56890cf86c91d7c9b
Viktor Holta <viktor.holta@outlook.com>
2024-03-06 11:10:22 +0100

    Modify docstring and add final timeout to request

--- src/backend_communication.py
+++ src/backend_communication.py
@@ -1,25 +1,23 @@
 """
-This module provides functions for communicating with the backend 
+This module provides functions for communicating with the backend
 and managing SBOMs (Software Bill of Materials).
 """
 
 import requests
+
 from util import Dependency
 
 
 def add_sbom(sbom_json: dict, dependencies: list[Dependency]):
     """
-    Adds a SBOM to the database
+    Adds a SBOM to the database.
+
     Args:
-        sbom_json: dict 
- 
-             An SBOM as a json object.
-        dependencies: list[Dependency]
-       The dependencies of the SBOM.
+        sbom_json (dict): An SBOM as a json object.
+        dependencies (list[Dependency]): The dependencies of the SBOM.
 
     Returns:
-        int:
-        Status code of the request
+        int: Status code of the request.
     """
     data = {}
     try:
@@ -37,48 +35,49 @@ def add_sbom(sbom_json: dict, dependencies: list[Dependency]):
         "checks": dep.dependency_score["checks"],
     } for dep in dependencies]
 
-    r = requests.post("localhost:5080/add_SBOM", json=data)
+    r = requests.post("localhost:5080/add_SBOM", json=data, timeout=5)
     return r.status_code
 
 
 # ossqa history
 def get_history():
     """
-    Gets previus SBOMs
-    Returns:
-        list:ious SBOM A list of the 
+    Gets previous SBOMs.
 
+    Returns:
+        list: The previous SBOMs.
     """
     response = requests.get("localhost:5080/get_history",  timeout=5)
     return response.json()
 
 
 # ossqa history [ID]
-def get_sbom(id: int):
+def get_sbom(sbom_id: int):
     """
-    Gets a specific SBOM
-    Args:
-        id:
-        int: The id of the SBOM
-    Returns:
+    Gets a specific SBOM.
 
+    Args:
+        sbom_id (int): The ID of the SBOM.
+
+    Returns:
+        dict: A dictionary-representation of the SBOM.
     """
     response = requests.get(
-        "localhost:5080/get_SBOM", 
-        data={"id": id}, 
+        "localhost:5080/get_SBOM",
+        data={"id": sbom_id},
         timeout=5)
     return response.json()
 
 
-def get_existing_dependencies(needed_dependencies: list):
+def get_existing_dependencies(needed_dependencies: list[Dependency]):
     """
-    Gets the existing dependencies
+    Gets the existing dependencies.
+
     Args:
-        needed_dependencies:
-        A list of the needed dependencies
+        needed_dependencies (list[Dependency]): The needed dependencies.
+
     Returns:
-        list:
-        A list of the existing dependencies as Dependency objects
+        list: A list of the existing dependencies.
     """
     dependency_primary_keys = []
     for depencency in needed_dependencies:
@@ -97,7 +96,7 @@ def get_existing_dependencies(needed_dependencies: list):
         url_split = depencency['name'].replace("https://", "").split("/")
         dep_obj = Dependency(
             dependency_score={
-                'score': depencency['score'], 
+                'score': depencency['score'],
                 'checks': depencency['checks']
                 },
             platform=url_split[0],
