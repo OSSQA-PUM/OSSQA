36c2a95a3e552d522dc1e13135affd4a6d963b39
jakob123100 <bokajnevs1@gmail.com>
2024-03-07 15:43:55 +0100

    Update calculator.py to conform to pep8

--- src/final_score_calculator/calculator.py
+++ src/final_score_calculator/calculator.py
@@ -3,7 +3,6 @@ This file contains a calculator for calculating final scores based on
 the scores of dependencies.
 """
 
-import json
 from util import Dependency, Checks
 
 # Temporary data types until we create more modules and proper data types
@@ -12,7 +11,7 @@ Score = list[str, int]  # (name, score)
 
 
 def calculate_final_scores(
-        dependencies: list[Dependency], 
+        dependencies: list[Dependency],
         requirements: list[float] = None
         ) -> list[FinalScore]:
     """
@@ -41,8 +40,10 @@ def calculate_final_scores(
     baseline: Dependency = dependencies[0]
     for check in Checks.all():
         current_baseline_check = baseline.get_check(check)
-        scores.append([check.value, current_baseline_check["score"], baseline.url])
-    
+        scores.append([check.value,
+                       current_baseline_check["score"],
+                       baseline.url])
+
     for dependency in dependencies:
         for score in scores:
             current_depency_score = dependency.get_check(score[0])["score"]
@@ -50,19 +51,5 @@ def calculate_final_scores(
                 # Replace current minimum score with dependency score
                 score[1] = current_depency_score
                 score[2] = dependency.url
-    
+
     return scores
-
-if __name__ == "__main__":
-    with open(
-        "E:/programming/OSSQA/src/final-score-calculator/example_repsonse.json",
-        encoding="utf-8"
-        ) as f:
-        score_card = json.load(f)
-    dependency: Dependency = Dependency(
-        json_component={}, url="test", dependency_score=score_card
-        )
-
-    dependencies = [dependency]
-
-    print(calculate_final_scores(dependencies))
