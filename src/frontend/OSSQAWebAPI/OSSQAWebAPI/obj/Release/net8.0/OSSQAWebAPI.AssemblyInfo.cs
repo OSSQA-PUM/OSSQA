f593561b1ce5a2ed6dca4cc511216f353aa9021b
Bacilika Glansholm <lunasotiz@gmail.com>
2024-04-18 17:26:13 +0200

    load bar

--- src/frontend/OSSQAWebAPI/OSSQAWebAPI/obj/Release/net8.0/OSSQAWebAPI.AssemblyInfo.cs
+++ src/frontend/OSSQAWebAPI/OSSQAWebAPI/obj/Release/net8.0/OSSQAWebAPI.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OSSQAWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce02b2627172cfdef5c749e210f34712473aa92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb368173373960fc1a956a5d2817d37420af259")]
 [assembly: System.Reflection.AssemblyProductAttribute("OSSQAWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OSSQAWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
