2ddee5d8c56f25b9ac4d094448f96a8da7336fc0
Jakob Söderström <bokajnevs1@gmail.com>
2024-04-23 21:25:39 +0200

    Update util.py

--- src/main/util.py
+++ src/main/util.py
@@ -3,7 +3,9 @@ This module contains utility functions for the main module.
 
 Functions:
 - get_git_sha1: Gets the SHA1 hash for a version of a dependency.
+- is_valid_sha1: Check the validity of a sha1 string.
 """
+import re
 
 
 def get_git_sha1(self, git_url: str, version: str) -> str:
@@ -19,5 +21,13 @@ def get_git_sha1(self, git_url: str, version: str) -> str:
     """
     sha1 = ""
     # TODO
-    # Add function to get git sha1 number
+    # Add function to get git sha1 number'
+    
+    assert is_valid_sha1(sha1), f"given commit sha1: {sha1} is not valid"
     return sha1
+
+
+def is_valid_sha1(sha1_str: str) -> bool:
+    if not re.match('^[0-9A-Fa-f]{40}$', sha1_str):
+        return False
+    return True
