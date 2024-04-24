3c480ca85c0cedd9965c3d27a29c51090fb93d3d
grufe <erikjosefsimonson@gmail.com>
2024-04-24 12:47:37 +0200

    Add more tests for DependencyManager class

--- src/tests/main/unit/data_types/sbom_types/test_dependency_manager.py
+++ src/tests/main/unit/data_types/sbom_types/test_dependency_manager.py
@@ -9,8 +9,8 @@ dictionary representation.
 
 The unit tests in this module cover the initialization of the
 `DependencyManager` class, the conversion of the manager to a dictionary,
-and the updating of dependencies.
-
+and the updating of dependencies. The tests also cover the retrieval of scored,
+unscored, and failed dependencies from the manager.
 """
 
 import json
@@ -19,11 +19,15 @@ from main.data_types.sbom_types.dependency_manager import DependencyManager
 from main.data_types.sbom_types.dependency import Dependency
 from main.data_types.sbom_types.scorecard import Scorecard
 from tests.main.unit.scorecards.scorecards import PATHS
-from tests.main.unit.data_types.sbom_types.expected_jsons.expected_results import PATHS as expected_paths
+from tests.main.unit.data_types.sbom_types.expected_jsons.expected_results \
+    import PATHS as expected_paths
 
 
 @pytest.fixture
 def dependency_manager_5_dependencies():
+    """
+    Fixture to create a DependencyManager with 5 dependencies.
+    """
     dep1 = Dependency(name="github.com/repo/path", version="1.0")
     dep2 = Dependency(name="github.com/repo/path", version="2.0")
     dep3 = Dependency(name="github.com/repo/path", version="3.0")
@@ -37,8 +41,11 @@ def dependency_manager_5_dependencies():
 
 @pytest.fixture
 def dependency_manager_with_score():
+    """
+    Fixture to create a DependencyManager with a scored dependency.
+    """
     dep1 = Dependency(name="github.com/repo/path", version="1.0")
-    with (open(PATHS[0], "r")) as file:
+    with (open(PATHS[0], "r", encoding="utf-8")) as file:
         scorecard = Scorecard(json.load(file))
     dep1.dependency_score = scorecard
     dependency_manager = DependencyManager()
@@ -48,6 +55,9 @@ def dependency_manager_with_score():
 
 @pytest.fixture
 def dependency_manager_failed_dependency():
+    """
+    Fixture to create a DependencyManager with a failed dependency.
+    """
     dep1 = Dependency(name="github.com/repo/path", version="1.0")
     dep1.failure_reason = Exception("Failed to fetch dependency")
     dependency_manager = DependencyManager()
@@ -56,9 +66,12 @@ def dependency_manager_failed_dependency():
 
 
 @pytest.fixture
-def dependency_manager_with_unscore_score_and_failed_dependency():
+def dep_mangr_with_distict_deps():
+    """
+    Fixture to create a DependencyManager with scored, unscored, and failed
+    dependencies."""
     dep1 = Dependency(name="github.com/repo/path", version="1.0")
-    with (open(PATHS[1], "r")) as file:
+    with (open(PATHS[1], "r", encoding="utf-8")) as file:
         scorecard = Scorecard(json.load(file))
     dep1.dependency_score = scorecard
     dep2 = Dependency(name="github.com/repo/path", version="2.0")
@@ -70,10 +83,16 @@ def dependency_manager_with_unscore_score_and_failed_dependency():
 
 
 def test_dependency_manager_initialization():
+    """
+    Test the initialization of the DependencyManager class.
+    """
     assert DependencyManager()
 
 
-def test_dependency_manager_to_dict():
+def test_dependency_empty_manager_to_dict():
+    """
+    Test the to_dict method of the DependencyManager class when the manager is
+    empty."""
     dependency_manager = DependencyManager()
     assert dependency_manager.to_dict() == {"scored_dependencies": [],
                                             "unscored_dependencies": [],
@@ -81,6 +100,9 @@ def test_dependency_manager_to_dict():
 
 
 def test_dependency_manager_update():
+    """
+    Test the update method of the DependencyManager class when the manager is
+    updated with a new dependency."""
     dependency_manager = DependencyManager()
     dep1 = Dependency(name="github.com/repo/path", version="1.0")
     dependency_manager.update([dep1])
@@ -89,24 +111,37 @@ def test_dependency_manager_update():
 
 def test_dependency_manager_get_scored_dependencies(
         dependency_manager_with_score):
+    """
+    Test the get_scored_dependencies method of the DependencyManager class when
+    the manager has scored dependencies."""
     assert len(dependency_manager_with_score.get_scored_dependencies()) == 1
 
 
 def test_dependency_manager_get_unscored_dependencies(
         dependency_manager_5_dependencies):
+    """
+    Test the get_unscored_dependencies method of the DependencyManager class 
+    when the manager has unscored dependencies."""
     dependency_manager = dependency_manager_5_dependencies
     assert len(dependency_manager.get_unscored_dependencies()) == 5
 
 
 def test_dependency_manager_get_failed_dependencies(
         dependency_manager_failed_dependency):
+    """
+    Test the get_failed_dependencies method of the DependencyManager class when
+    the manager has a failed dependency."""
     dependency_manager = dependency_manager_failed_dependency
     assert len(dependency_manager.get_failed_dependencies()) == 1
 
 
 def test_dependency_manager_get_unscored_scored_and_failed_dependencies(
-        dependency_manager_with_unscore_score_and_failed_dependency):
-    dependency_manager = dependency_manager_with_unscore_score_and_failed_dependency
+        dep_mangr_with_distict_deps):
+    """
+    Test the get_unscored_dependencies, get_scored_dependencies, and
+    get_failed_dependencies methods of the DependencyManager class when the
+    manager has scored, unscored, and failed dependencies."""
+    dependency_manager = dep_mangr_with_distict_deps
     assert len(dependency_manager.get_unscored_dependencies()) == 1
     assert len(dependency_manager.get_scored_dependencies()) == 1
     assert len(dependency_manager.get_failed_dependencies()) == 1
@@ -114,14 +149,25 @@ def test_dependency_manager_get_unscored_scored_and_failed_dependencies(
 
 def test_dependency_manager_update_same_dependency(
         dependency_manager_5_dependencies):
+    """
+    Test the update method of the DependencyManager class when the same
+    dependency is updated multiple times."""
     dependency_manager = dependency_manager_5_dependencies
     new_dep = Dependency(name="github.com/repo/path", version="5.0")
+    dependency_manager.update([new_dep])
+    dependency_manager.update([new_dep])
+    dependency_manager.update([new_dep])
+    dependency_manager.update([new_dep])
     dependency_manager.update([new_dep])
     assert len(dependency_manager.get_unscored_dependencies()) == 5
 
 
 def test_dependency_manager_replace_scored_with_unscored(
         dependency_manager_with_score):
+    """
+    Test the update method of the DependencyManager class when a scored
+    dependency is replaced with an unscored dependency.
+    """
     dependency_manager = dependency_manager_with_score
     new_dep = Dependency(name="github.com/repo/path", version="1.0")
     dependency_manager.update([new_dep])
@@ -130,11 +176,47 @@ def test_dependency_manager_replace_scored_with_unscored(
 
 
 def test_dependency_manager_to_dict_filled(
-        dependency_manager_with_unscore_score_and_failed_dependency
+        dep_mangr_with_distict_deps
 ):
-    dependency_manager = dependency_manager_with_unscore_score_and_failed_dependency
-    with (open(expected_paths[0], "r")) as file:
+    """
+    Test the to_dict method of the DependencyManager class when the manager
+    has scored, unscored, and failed dependencies.
+    """
+    dependency_manager = dep_mangr_with_distict_deps
+    with (open(expected_paths[0], "r", encoding="utf-8")) as file:
         expected = json.load(file)
-    assert dependency_manager.to_dict()["scored_dependencies"][0]["dependency_score"] == expected
-    assert dependency_manager.to_dict()["unscored_dependencies"] == [{'name': 'github.com/repo/path', 'version': '3.0', 'dependency_score': '', 'failure_reason': ''}]
-    assert isinstance(dependency_manager.to_dict()["failed_dependencies"][0]["failure_reason"], Exception)
+    dep_dict = dependency_manager.to_dict()
+    assert dep_dict["scored_dependencies"][0]["dependency_score"] == expected
+    assert dep_dict["unscored_dependencies"] == \
+        [{'name': 'github.com/repo/path',
+          'version': '3.0',
+          'dependency_score': '',
+          'failure_reason': ''}]
+    assert isinstance(
+        dep_dict["failed_dependencies"][0]["failure_reason"], Exception
+        )
+
+
+def test_dependency_manager_get_by_filter(dep_mangr_with_distict_deps):
+    """
+    Test the get_dependencies_by_filter method of the DependencyManager class.
+    """
+    dependency_manager = dep_mangr_with_distict_deps
+
+    scored_deps = dependency_manager.get_dependencies_by_filter(
+        lambda dep: dep.dependency_score
+    )
+    unscored_deps = dependency_manager.get_dependencies_by_filter(
+        lambda dep: not dep.dependency_score
+    )
+    failed_deps = dependency_manager.get_dependencies_by_filter(
+        lambda dep: dep.failure_reason
+    )
+    all_deps = dependency_manager.get_dependencies_by_filter(
+        lambda dep: True
+    )
+
+    assert len(scored_deps) == 1
+    assert len(unscored_deps) == 2
+    assert len(failed_deps) == 1
+    assert len(all_deps) == 3
