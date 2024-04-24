2eb1c8e2b49f4d5b1042b6ee16a8e28c055a793e
grufe <erikjosefsimonson@gmail.com>
2024-04-24 19:56:03 +0200

    Refactor Dockerfiles and fix web_api.py to now receieve an SBOM from the program

--- src/main/frontend/web_api.py
+++ src/main/frontend/web_api.py
@@ -1,11 +1,13 @@
-from flask import Flask, request
 import json
-import front_end_api
+from flask import Flask, request
+from main.frontend.front_end_api import FrontEndAPI
 from main.data_types.sbom_types.sbom import Sbom
-from main.data_types.user_requirements import UserRequirements
+from main.data_types.user_requirements import (UserRequirements,
+                                               RequirementsType)
+from main.sbom_processor import SbomProcessorStatus
 
 app = Flask(__name__)
-frontend_api = front_end_api.FrontEndAPI()
+frontend_api = FrontEndAPI()
 status: str = "Idle"
 
 
@@ -27,13 +29,25 @@ def analyze():
     try:
         user_reqs_param = data['user_reqs']
     except KeyError:
-        user_reqs_param = "[10,10,10,10,10]"
-    user_reqs: UserRequirements = UserRequirements(user_reqs_param)
-    print(user_reqs.to_dict())
-    sbom = Sbom.from_dict(data['sbom'])
+        user_reqs_param = "[10, 10, 10, 10, 10]"
+    user_reqs_param = json.loads(user_reqs_param)
+    user_reqs_dict = {
+        RequirementsType.CODE_VULNERABILITIES: user_reqs_param[0],
+        RequirementsType.MAINTENANCE: user_reqs_param[1],
+        RequirementsType.CONTINUOUS_TESTING: user_reqs_param[2],
+        RequirementsType.SOURCE_RISK_ASSESSMENT: user_reqs_param[3],
+        RequirementsType.BUILD_RISK_ASSESSMENT: user_reqs_param[4]
+        }
+    
+    try:
+        user_reqs: UserRequirements = UserRequirements(user_reqs_dict)
+    except (AssertionError, ValueError):
+        return "Invalid user requirements", 415
+    
+    sbom = Sbom(json.loads(data['sbom']))
     print("\n\n\n")
     print(sbom.to_dict())
-    frontend_api.subscribe_to_state_change(lambda x: update_current_status(x))
+    frontend_api.subscribe_to_state_change(update_current_status)
     result_sbom: Sbom = frontend_api.analyze_sbom(sbom, user_reqs)
     result_json = result_sbom.to_dict()
     return json.dumps(result_json)
@@ -45,12 +59,11 @@ def hello():
     return "Hello World!"
 
 
-def update_current_status(update: str):
+def update_current_status(update: SbomProcessorStatus):
     """
     Updates the current status of the request.
     """
-    global status
-    status = update
+    print(f"Status updated: {update}")
 
 
 @app.route("/get_current_status", methods=['GET'])
@@ -58,4 +71,5 @@ def get_current_status():
     return status
 
 
-app.run(port=98, debug=True, host='0.0.0.0')
+def run():
+    app.run(port=98, debug=True, host='0.0.0.0')
