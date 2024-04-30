0b46a151dd5a7302c36f591ecfee1182818de29a
Viktor Holta <viktor.holta@outlook.com>
2024-04-30 16:13:34 +0200

    Add TODO about fixing Dependency data type

--- src/main/data_types/sbom_types/sbom.py
+++ src/main/data_types/sbom_types/sbom.py
@@ -145,6 +145,15 @@ class Sbom:
         name = ""
         version = ""
         try:
+            # TODO: The name of the component is not in the url.
+            #       According to the CycloneDX documentation,
+            #       each component has a name that can be accessed via
+            #       component["name"].
+            #       This should be fixed, preferably by storing the
+            #       URL in it's own variable in Dependency, so
+            #       that OSSQA adheres to the CycloneDX format,
+            #       which in turn makes the behavior of the program
+            #       more predictable.
             name = self._parse_git_url(
                 self._get_component_url(component=component)
                 )
