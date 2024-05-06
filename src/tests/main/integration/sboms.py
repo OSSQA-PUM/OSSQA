789e79faae0698213a8ec754c12064b7599f3edf
Viktor Holta <viktor.holta@outlook.com>
2024-05-06 22:32:32 +0200

    Create downscaled SBOM to use in integration tests

---
+++ src/tests/main/integration/sboms.py
@@ -0,0 +1,243 @@
+"""
+This module contains downscaled SBOMs tailored to be used in
+the integration tests.
+"""
+from main.data_types.sbom_types.sbom import Sbom
+from main.data_types.sbom_types.scorecard import Scorecard
+
+SMALL_SBOM = Sbom({
+  "bomFormat": "CycloneDX",
+  "specVersion": "1.2",
+  "serialNumber": "urn:uuid:d7a0ac67-e0f8-4342-86c6-801a02437636",
+  "version": 1,
+  "metadata": {
+    "timestamp": "2021-05-16T17:10:53+02:00",
+    "component": {
+      "bom-ref": "pkg:golang/github.com/ProtonMail/proton-bridge@v1.8.0",
+      "type": "application",
+      "name": "github.com/ProtonMail/proton-bridge",
+      "version": "v1.8.0",
+      "purl": "pkg:golang/github.com/ProtonMail/proton-bridge@v1.8.0",
+      "externalReferences": [
+        {
+          "url": "https://github.com/ProtonMail/proton-bridge",
+          "type": "vcs"
+        }
+      ]
+    }
+  },
+  "components": [
+    {
+      "bom-ref": "pkg:golang/github.com/andybalholm/cascadia@v1.1.0",
+      "type": "library",
+      "name": "github.com/andybalholm/cascadia",
+      "version": "v1.1.0",
+      "scope": "required",
+      "hashes": [
+        {
+          "alg": "SHA-256",
+          "content": "06eb8eeac49f40d151bb52e9a606c3db91ebdaf2d85b6e49bf11ece73cec2d3a"
+        }
+      ],
+      "licenses": [
+        {
+          "license": {
+            "id": "BSD-2-Clause",
+            "url": "https://spdx.org/licenses/BSD-2-Clause.html"
+          }
+        }
+      ],
+      "purl": "pkg:golang/github.com/andybalholm/cascadia@v1.1.0",
+      "externalReferences": [
+        {
+          "url": "https://github.com/andybalholm/cascadia",
+          "type": "vcs"
+        }
+      ]
+    }
+  ]
+})
+SMALL_SCORECARD_DICT = {
+    "date": "2024-04-15",
+    "repo": {
+        "name": "github.com/jonschlinkert/pad-left",
+        "commit": "e521c7ba0d5d290b2cef7485af11b98dc96f2930"
+    },
+    "scorecard": {
+        "version": "v4.13.1-273-g0b9dfb65",
+        "commit": "0b9dfb656f1796c7c693ad74f5193657b6a81e0b"
+    },
+    "score": 3.2,
+    "checks": [
+        {
+            "name": "Code-Review",
+            "score": 1,
+            "reason": "Found 3/28 approved changesets -- score normalized to 1",
+            "details": None,
+            "documentation": {
+                "short": "Determines if the project requires human code review before pull requests (aka merge requests) are merged.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#code-review"
+            }
+        },
+        {
+            "name": "Maintained",
+            "score": 0,
+            "reason": "0 commit(s) and 0 issue activity found in the last 90 days -- score normalized to 0",
+            "details": None,
+            "documentation": {
+                "short": "Determines if the project is \"actively maintained\".",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#maintained"
+            }
+        },
+        {
+            "name": "CII-Best-Practices",
+            "score": 0,
+            "reason": "no effort to earn an OpenSSF best practices badge detected",
+            "details": None,
+            "documentation": {
+                "short": "Determines if the project has an OpenSSF (formerly CII) Best Practices Badge.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#cii-best-practices"
+            }
+        },
+        {
+            "name": "License",
+            "score": 10,
+            "reason": "license file detected",
+            "details": [
+                "Info: project has a license file: LICENSE:0",
+                "Info: FSF or OSI recognized license: MIT License: LICENSE:0"
+            ],
+            "documentation": {
+                "short": "Determines if the project has defined a license.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#license"
+            }
+        },
+        {
+            "name": "Signed-Releases",
+            "score": -1,
+            "reason": "no releases found",
+            "details": None,
+            "documentation": {
+                "short": "Determines if the project cryptographically signs release artifacts.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#signed-releases"
+            }
+        },
+        {
+            "name": "Token-Permissions",
+            "score": -1,
+            "reason": "No tokens found",
+            "details": None,
+            "documentation": {
+                "short": "Determines if the project's workflows follow the principle of least privilege.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#token-permissions"
+            }
+        },
+        {
+            "name": "Dangerous-Workflow",
+            "score": -1,
+            "reason": "no workflows found",
+            "details": None,
+            "documentation": {
+                "short": "Determines if the project's GitHub Action workflows avoid dangerous patterns.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#dangerous-workflow"
+            }
+        },
+        {
+            "name": "Packaging",
+            "score": -1,
+            "reason": "packaging workflow not detected",
+            "details": [
+                "Warn: no GitHub/GitLab publishing workflow detected."
+            ],
+            "documentation": {
+                "short": "Determines if the project is published as a package that others can easily download, install, easily update, and uninstall.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#packaging"
+            }
+        },
+        {
+            "name": "Binary-Artifacts",
+            "score": 10,
+            "reason": "no binaries found in the repo",
+            "details": None,
+            "documentation": {
+                "short": "Determines if the project has generated executable (binary) artifacts in the source repository.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#binary-artifacts"
+            }
+        },
+        {
+            "name": "Pinned-Dependencies",
+            "score": -1,
+            "reason": "no dependencies found",
+            "details": None,
+            "documentation": {
+                "short": "Determines if the project has declared and pinned the dependencies of its build process.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#pinned-dependencies"
+            }
+        },
+        {
+            "name": "Branch-Protection",
+            "score": 0,
+            "reason": "branch protection not enabled on development/release branches",
+            "details": [
+                "Warn: branch protection not enabled for branch 'master'"
+            ],
+            "documentation": {
+                "short": "Determines if the default and release branches are protected with GitHub's branch protection settings.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#branch-protection"
+            }
+        },
+        {
+            "name": "Fuzzing",
+            "score": 0,
+            "reason": "project is not fuzzed",
+            "details": [
+                "Warn: no fuzzer integrations found"
+            ],
+            "documentation": {
+                "short": "Determines if the project uses fuzzing.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#fuzzing"
+            }
+        },
+        {
+            "name": "Vulnerabilities",
+            "score": 10,
+            "reason": "0 existing vulnerabilities detected",
+            "details": None,
+            "documentation": {
+                "short": "Determines if the project has open, known unfixed vulnerabilities.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#vulnerabilities"
+            }
+        },
+        {
+            "name": "Security-Policy",
+            "score": 0,
+            "reason": "security policy file not detected",
+            "details": [
+                "Warn: no security policy file detected",
+                "Warn: no security file to analyze",
+                "Warn: no security file to analyze",
+                "Warn: no security file to analyze"
+            ],
+            "documentation": {
+                "short": "Determines if the project has published a security policy.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#security-policy"
+            }
+        },
+        {
+            "name": "SAST",
+            "score": 0,
+            "reason": "SAST tool is not run on all commits -- score normalized to 0",
+            "details": [
+                "Warn: 0 commits out of 5 are checked with a SAST tool"
+            ],
+            "documentation": {
+                "short": "Determines if the project uses static code analysis.",
+                "url": "https://github.com/ossf/scorecard/blob/0b9dfb656f1796c7c693ad74f5193657b6a81e0b/docs/checks.md#sast"
+            }
+        }
+    ]
+}
+
+for dependency in SMALL_SBOM.dependency_manager.get_unscored_dependencies():
+    dependency.dependency_score = Scorecard(SMALL_SCORECARD_DICT)
+    SMALL_SBOM.dependency_manager.update([dependency])
--- src/tests/main/integration/test_get_named_sboms.py
+++ src/tests/main/integration/test_get_named_sboms.py
@@ -2,9 +2,7 @@
 This module tests the functionality of gettings SBOMs with specific names
 from the database.
 """
-import json
 import sys
-from pathlib import Path
 from unittest.mock import patch
 
 import pytest
@@ -13,45 +11,16 @@ import requests
 from main.backend_communication import BackendCommunication
 from main.data_types.dependency_scorer import StepResponse
 from main.data_types.sbom_types.sbom import Sbom
-from main.data_types.sbom_types.scorecard import Scorecard
 from main.frontend.cli import ossqa_cli
 from main.frontend.front_end_api import FrontEndAPI
 from main.sbom_processor import SbomProcessor, SbomProcessorStatus
 
-from tests.main.unit.sboms.sboms import PATHS as SBOM_PATHS
-from tests.main.unit.scorecards.scorecards import PATHS as SCORECARD_PATHS
+# TODO: use in other integration tests with prev integration test branch
+#       is merged
+from tests.main.integration.sboms import SMALL_SBOM
 
 # TODO: use constanst module when prev integration test branch is merged
 HOST = "http://localhost:5091"
-
-
-@pytest.fixture(name="sbom_path", scope="module")
-def sbom_path_fixture() -> Path:
-    return SBOM_PATHS[0]
-
-
-@pytest.fixture(name="fake_scored_sbom", scope="module")
-def fake_scored_sbom_fixture(sbom_path: Path) -> Sbom:
-    """
-    This fixture reads and SBOM file and creates an Sbom object
-    with fake scorecard data.
-    """
-    with open(sbom_path, "r", encoding="utf-8") as sbom_file:
-        sbom = Sbom(json.load(sbom_file))
-
-    unscored_deps = sbom.dependency_manager.get_unscored_dependencies()
-    max_idx = min(len(unscored_deps), len(SCORECARD_PATHS))
-    scored_deps = []
-
-    for idx in range(max_idx):
-        with open(SCORECARD_PATHS[idx], "r", encoding="utf-8") as file:
-            scorecard = Scorecard(json.load(file))
-        dependency = unscored_deps[idx]
-        dependency.dependency_score = scorecard
-        scored_deps.append(dependency)
-
-    sbom.dependency_manager.update(scored_deps)
-    return sbom
 
 
 @pytest.mark.order(-1) # Ensures the tests run after all unit tests
@@ -60,15 +29,15 @@ class TestGetNamedSboms:
     These functions test the action of getting SBOMs with specific names
     from the database in a bottom-up fashion.
     """
-    def test_populate_database(self, fake_scored_sbom: Sbom):
+    def test_populate_database(self):
         resp = requests.post(HOST + "/test/reset", timeout=10)
         assert resp.status_code == 200
-        sbom_dict = fake_scored_sbom.to_dict()
+        sbom_dict = SMALL_SBOM.to_dict()
         resp = requests.post(HOST + "/sbom", json=sbom_dict, timeout=10)
         assert resp.status_code == 201
 
-    def test_backend(self, fake_scored_sbom: Sbom):
-        name = fake_scored_sbom.repo_name
+    def test_backend(self):
+        name = SMALL_SBOM.repo_name
         resp = requests.get(HOST + f"/sbom/{name}", timeout=5)
         assert resp.status_code == 200
         sbom_dicts = resp.json()
@@ -76,8 +45,8 @@ class TestGetNamedSboms:
         sbom = Sbom(sbom_dicts[0])
         assert sbom.repo_name == name
 
-    def test_backend_comm(self, fake_scored_sbom: Sbom):
-        name = fake_scored_sbom.repo_name
+    def test_backend_comm(self):
+        name = SMALL_SBOM.repo_name
         def callback(response: StepResponse):
             assert response.message != "The request timed out"
             assert response.message != "An error occurred in the database"
@@ -87,8 +56,8 @@ class TestGetNamedSboms:
         assert len(sboms) != 0
         assert sboms[0].repo_name == name
 
-    def test_sbom_processor(self, fake_scored_sbom: Sbom):
-        name = fake_scored_sbom.repo_name
+    def test_sbom_processor(self):
+        name = SMALL_SBOM.repo_name
         def callback(status: SbomProcessorStatus):
             if response := status.step_response:
                 assert response.message != "The request timed out"
@@ -101,8 +70,8 @@ class TestGetNamedSboms:
         assert sboms[0].repo_name == name
 
 
-    def test_front_end_api(self, fake_scored_sbom: Sbom):
-        name = fake_scored_sbom.repo_name
+    def test_front_end_api(self):
+        name = SMALL_SBOM.repo_name
         def callback(status: SbomProcessorStatus):
             if response := status.step_response:
                 assert response.message != "The request timed out"
@@ -115,8 +84,8 @@ class TestGetNamedSboms:
         assert sboms[0].repo_name == name
 
 
-    def test_cli(self, fake_scored_sbom: Sbom):
-        name = fake_scored_sbom.repo_name
+    def test_cli(self):
+        name = SMALL_SBOM.repo_name
         # Temporarily overwrite sys.argv, then run the CLI
         mock_args = ["prog", "lookup", "--backend", HOST, name]
         with patch("sys.argv", mock_args):
