eafb31064fdc192df0789f542cd0e06e1d8648b1
Simon Karlsson <simonkarlsson020314@gmail.com>
2024-05-10 13:25:22 +0200

    Removed unnecessary print

--- src/main/frontend/final_score_calculator.py
+++ src/main/frontend/final_score_calculator.py
@@ -18,16 +18,14 @@ def calculate_final_scores(sbom: Sbom, user_requirements: UserRequirements) -> S
         Sbom: The SBOM with the final scores calculated.
     """
     sbom_copy = copy.deepcopy(sbom)
+    listed_requirements = user_requirements.get_listed_requirements()
     checks = []
     check_names = []
     names_score = []
 
-    #print("Old dependencies: \n" + str(sbom_copy.dependency_manager.get_scored_dependencies()))
-
     for dependency in sbom_copy.dependency_manager.get_scored_dependencies():
-        listed_requirements = user_requirements.get_listed_requirements()
-        for ch in dependency.dependency_score.checks:
-            check_names.append(ch.name)
+        for check in dependency.dependency_score.checks:
+            check_names.append(check.name)
 
         for check in dependency.dependency_score.checks:
             names_score.append(check.name)
@@ -41,17 +39,15 @@ def calculate_final_scores(sbom: Sbom, user_requirements: UserRequirements) -> S
                 if requirement[1] > -1:
                     dependency.reach_requirement = "Test result not found"
             else:
-                for check1 in checks:
-                    if check1[0] == requirement[0]:
-                        if (requirement[1] <= check1[1] and
+                for check in checks:
+                    if check[0] == requirement[0]:
+                        if (requirement[1] <= check[1] and
                             dependency.reach_requirement != "No" and 
                             dependency.reach_requirement != "Test result not found"):
                             dependency.reach_requirement = "Yes"
                         else:
-                            if requirement[1] > check1[1]:
+                            if requirement[1] > check[1]:
                                 dependency.reach_requirement = "No"
-                
         checks.clear()
-
-    # TODO: Implement final score calculation
+        
     return sbom_copy
