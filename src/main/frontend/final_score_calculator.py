eb590fc8ac1239374fc79bf717cd91c70865be7d
Simon Karlsson <simonkarlsson020314@gmail.com>
2024-05-10 13:16:38 +0200

    Actually working code without the bug

--- src/main/frontend/final_score_calculator.py
+++ src/main/frontend/final_score_calculator.py
@@ -19,27 +19,37 @@ def calculate_final_scores(sbom: Sbom, user_requirements: UserRequirements) -> S
     """
     sbom_copy = copy.deepcopy(sbom)
     checks = []
+    check_names = []
+    names_score = []
 
     #print("Old dependencies: \n" + str(sbom_copy.dependency_manager.get_scored_dependencies()))
 
     for dependency in sbom_copy.dependency_manager.get_scored_dependencies():
         listed_requirements = user_requirements.get_listed_requirements()
+        for ch in dependency.dependency_score.checks:
+            check_names.append(ch.name)
+
         for check in dependency.dependency_score.checks:
-                checks.append(check.name)
+            names_score.append(check.name)
+            names_score.append(check.score)
+            copy_names_score = names_score.copy()
+            checks.append(copy_names_score)
+            names_score.clear()
+
         for requirement in listed_requirements:
-            if not requirement[0] in checks:
+            if not requirement[0] in check_names:
                 if requirement[1] > -1:
                     dependency.reach_requirement = "Test result not found"
             else:
                 for check1 in checks:
-                    if check1 == requirement[0]:
-                        if (requirement[1] <= dependency.dependency_score.checks[0].score and
+                    if check1[0] == requirement[0]:
+                        if (requirement[1] <= check1[1] and
                             dependency.reach_requirement != "No" and 
                             dependency.reach_requirement != "Test result not found"):
                             dependency.reach_requirement = "Yes"
                         else:
-                            print("Req: " + str(requirement[1]) + " for " + str(requirement[0]) + " and score: " + str(dependency.dependency_score.checks[0].score) + " for " + str(dependency.dependency_score.checks[0].name))
-                            dependency.reach_requirement = "No"
+                            if requirement[1] > check1[1]:
+                                dependency.reach_requirement = "No"
                 
         checks.clear()
 
