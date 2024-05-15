5d0e3869ec6201705f28d974f47db043d8821e54
Viktor Holta <viktor.holta@outlook.com>
2024-05-15 20:03:21 +0200

    Fix and add docstrings in sbom.py

--- src/main/data_types/sbom_types/sbom.py
+++ src/main/data_types/sbom_types/sbom.py
@@ -24,6 +24,7 @@ class Sbom:
         version (str): The version of the SBOM.
         repo_name (str): The name of the repository.
         repo_version (str): The version of the repository.
+        spec_version (str): The specification version of the SBOM.
     """
     dependency_manager: DependencyManager
     serial_number: str
@@ -33,6 +34,12 @@ class Sbom:
     spec_version: str
 
     def __init__(self, sbom: dict):
+        """
+        Initializes the SBOM.
+
+        Args:
+            sbom (dict): The SBOM contents.
+        """
         self._check_format_of_sbom(sbom)
         self.dependency_manager: DependencyManager \
             = DependencyManager(sbom["components"])
@@ -45,10 +52,10 @@ class Sbom:
 
     def to_dict(self) -> dict:
         """
-        Converts the SBOM object to a dictionary.
+        Creates a dictionary representing the SBOM.
 
         Returns:
-            dict: The SBOM object as a dictionary.
+            dict: The SBOM as a dictionary.
         """
         res = {}
         res["serialNumber"] = self.serial_number
@@ -62,13 +69,14 @@ class Sbom:
             res[key] = value
 
         return res
-    
+
     def to_dict_web(self) -> dict:
         """
-        Converts the SBOM object to a dictionary.
+        Creates a dictionary representing the SBOM to use in the web
+        interface.
 
         Returns:
-            dict: The SBOM object as a dictionary.
+            dict: The SBOM as a dictionary.
         """
         res = {}
         res["serialNumber"] = self.serial_number
@@ -85,38 +93,38 @@ class Sbom:
 
     def get_scored_dependencies(self) -> list[Dependency]:
         """
-        Gets the dependencies of the SBOM.
+        Gets the scored dependencies of the SBOM.
 
         Returns:
-            list[Dependency]: The dependencies of the SBOM.
+            list[Dependency]: The scored dependencies of the SBOM.
         """
         return self.dependency_manager.get_scored_dependencies()
 
     def get_unscored_dependencies(self) -> list[Dependency]:
         """
-        Gets the dependencies of the SBOM.
+        Gets the unscored dependencies of the SBOM.
 
         Returns:
-            list[Dependency]: The dependencies of the SBOM.
+            list[Dependency]: The unscored dependencies of the SBOM.
         """
         return self.dependency_manager.get_unscored_dependencies()
 
     def get_failed_dependencies(self) -> list[Dependency]:
         """
-        Gets the dependencies of the SBOM.
+        Gets the failed dependencies of the SBOM.
 
         Returns:
-            list[Dependency]: The dependencies of the SBOM.
+            list[Dependency]: The failed dependencies of the SBOM.
         """
         return self.dependency_manager.get_failed_dependencies()
 
     def get_dependencies_by_filter(self, dependency_filter: callable) \
             -> list[Dependency]:
         """
-        Gets the dependencies of the SBOM.
+        Gets the dependencies of the SBOM with a filter.
 
         Returns:
-            list[Dependency]: The dependencies of the SBOM.
+            list[Dependency]: The filtered dependencies of the SBOM.
         """
         return self.dependency_manager.get_dependencies_by_filter(
             dependency_filter
@@ -131,32 +139,28 @@ class Sbom:
         """
         self.dependency_manager.update(dependencies)
 
-    def _check_format_of_sbom(self, sbom_file: dict) -> None:
+    def _check_format_of_sbom(self, sbom_contents: dict) -> None:
         """
-        Checks the format of the SBOM file.
+        Checks the format of the SBOM contents.
 
         Args:
-            sbom_file (dict): The SBOM file to be checked.
+            sbom_contents (dict): The SBOM contents to be checked.
 
         Raises:
             SyntaxError: If the 'bomFormat' is missing or not 'CycloneDX'.
-
             IndexError: If the 'specVersion' is missing, out of date, or
                         incorrect.
-
             SyntaxError: If the 'serialNumber' does not match the RFC-4122
                          format.
-
             IndexError: If the 'version' of SBOM is lower than 1
             or not a proper integer.
-
             ValueError: If the name could not be found, indicating a non-valid
                         SBOM.
         """
-        if not sbom_file["bomFormat"] == "CycloneDX":
+        if not sbom_contents["bomFormat"] == "CycloneDX":
             raise SyntaxError("bomFormat missing or not CycloneDX")
 
-        if not sbom_file["specVersion"] in ["1.2", "1.3", "1.4", "1.5"]:
+        if not sbom_contents["specVersion"] in ["1.2", "1.3", "1.4", "1.5"]:
             raise IndexError(
                 "CycloneDX version missing, out of date or incorrect"
                 )
@@ -164,19 +168,19 @@ class Sbom:
         if not match(
                 "^urn:uuid:[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-" +
                 "[0-9a-f]{4}-[0-9a-f]{12}$",
-                sbom_file["serialNumber"]):
+                sbom_contents["serialNumber"]):
             raise SyntaxError(
                 "SBOM Serial number does not match the RFC-4122 format")
 
-        if not sbom_file["version"] >= 1:
+        if not sbom_contents["version"] >= 1:
             raise IndexError("Version of SBOM is lower than 1")
 
-        if not isinstance(sbom_file["version"], int):
+        if not isinstance(sbom_contents["version"], int):
             raise IndexError("Version of SBOM is not proper integer")
 
         # Checks if name of SBOM exists
         try:
-            name = sbom_file["metadata"]["component"]["name"]
+            name = sbom_contents["metadata"]["component"]["name"]
         except (IndexError, KeyError):
             name = ""
 
